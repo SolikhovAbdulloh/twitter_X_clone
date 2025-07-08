8b0644d9b2f09e971d480832916997c511d158c7
Abdulloh <abdullasolikh@gmail.com>
2025-07-08 17:58:31 +0500

    login succsess

--- components/modals/registerModal.jsx
+++ components/modals/registerModal.jsx
@@ -1,29 +1,116 @@
 import useRegisterModal from "@/hook/useRegistonModal";
-import React from "react";
+import React, { useState } from "react";
 import Modal from "../ui/modal";
-import { X } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import Button from "../ui/button";
+import { RegisterSchema_1 } from "@/lib/validation";
 
 function RegisterModal() {
+  const [step, setstep] = useState(1);
+  const [data, setData] = useState({ name: "", email: "" });
   const registerModal = useRegisterModal();
-  const body = (
-    <div className="flex flex-col">
-      <button
-        onClick={registerModal.onClose}
-        className="p-2 hover:opacity-70 transition w-fit border-0 cursor-pointer"
-      >
-        <X size={28} />
-      </button>
+  const body =
+    step === 1 ? (
+      <Register_1 setData={setData} setstep={setstep} />
+    ) : (
+      <Register_2 />
+    );
+  const footer = (
+    <div>
+      <p className="text-neutral-400 text-center mb-4">
+        Already have an account ?{" "}
+        <span className="text-white hover:underline cursor-pointer">
+          Sign in
+        </span>
+      </p>
     </div>
   );
-  const footer = <div>footer Content</div>;
   return (
     <Modal
       body={body}
       footer={footer}
       isOpen={registerModal.isOpen}
       Onclose={registerModal.onClose}
+      step={step}
+      totalStep={2}
     />
   );
+}
+function Register_1({ setData, setstep }) {
+  const onSubmit = (data) => {
+    console.log("Form data:", data);
+    setData(data);
+    setstep(2);
+  };
+  const form = useForm({
+    resolver: zodResolver(RegisterSchema_1),
+    defaultValues: {
+      email: "",
+      name: "",
+    },
+  });
+  const { isSubmitting } = form.formState;
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="email" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          secondary
+          fullWidth
+          type="submit"
+          label="Next"
+          disabled={isSubmitting}
+        >
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+}
+function Register_2() {
+  // const form = useForm({
+  //   resolver: zodResolver(RegisterSchema_2),
+  // });
+  return <div>Register_2</div>;
 }
 
 export default RegisterModal;
--- components/ui/modal.jsx
+++ components/ui/modal.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./dialog";
+import { Dialog, DialogContent } from "./dialog";
+import { X } from "lucide-react";
 
-function Modal({ body, footer, isOpen, Onclose }) {
+function Modal({ body, footer, isOpen, Onclose, step, totalStep }) {
   return (
     <Dialog open={isOpen} onOpenChange={Onclose}>
       <DialogContent className="bg-[black] opacity-80">
+        <button
+          onClick={Onclose}
+          className="p-2 hover:opacity-70 transition w-fit border-0 cursor-pointer"
+        >
+          <X size={28} />
+        </button>
+        {step && totalStep && (
+          <div className="text-xl font-bold">
+            Step {step} of {totalStep}
+          </div>
+        )}
         <div className="mt-4">{body}</div>
         {footer && <div className="mt-4">{footer}</div>}
       </DialogContent>
